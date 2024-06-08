f45e4c8f7a23fac72374177159f5a1f41524ba8f
Nina <home261@yandex.ru>
2024-06-08 19:26:50 +0300

    My first commit

---
+++ configuration.py
@@ -0,0 +1,12 @@
+
+URL_SERVICE = "https://a29756c3-9ca4-457f-9d02-da6d335dc386.serverhub.praktikum-services.ru"
+
+DOC_PATH = "/docs/"
+
+LOG_MAIN_PATH = "/api/logs/main/"
+
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+
+CREATE_USER_PATH = "/api/v1/users/"
+
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
---
+++ sender_stand_request.py
@@ -0,0 +1,20 @@
+import configuration
+import requests
+import data
+
+# Определение функции post_new_user для отправки POST-запроса на создание нового пользователя
+def post_new_user(body):
+    # Выполнение POST-запроса с использованием URL из конфигурационного файла, тела запроса и заголовков
+    # URL_SERVICE и CREATE_USER_PATH объединяются для формирования полного URL для запроса
+    # json=body используется для отправки данных пользователя в формате JSON
+    # headers=data.headers устанавливает заголовки запроса из модуля data
+    return requests.post(configuration.URL_SERVICE + configuration.CREATE_USER_PATH,
+                         json=body,
+                         headers=data.headers)
+
+# Вызов функции post_new_user с телом запроса для создания нового пользователя из модуля data
+    response = post_new_user(data.user_body);
+    print(response.status_code)
+    print(response.json())
+
+
